640c568a19cdc041a670c117efa694977c219797
Ruslan Borisov <borisov.rusya@gmail.com>
2014-09-13 01:26:27 +0300

    DateParser upgrade. Some fix

--- Service/src/com/diosoft/calendar/server/util/DateParser.java
+++ Service/src/com/diosoft/calendar/server/util/DateParser.java
@@ -1,14 +1,20 @@
 package com.diosoft.calendar.server.util;
 
 import com.diosoft.calendar.server.exception.DateTimeFormatException;
+import org.apache.log4j.Logger;
 
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class DateParser {
 
-    public static LocalDateTime stringToDate(String stringDate) throws DateTimeFormatException {
+    private static final Logger LOG = Logger.getLogger(DateParser.class);
+
+    public static LocalDateTime stringToDate(String stringDate) throws DateTimeFormatException, IllegalArgumentException {
+
+        if (stringDate==null) throw new IllegalArgumentException();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime dateTime = null;
@@ -16,20 +22,25 @@ public class DateParser {
         try {
             dateTime = LocalDateTime.parse(stringDate, formatter);
         } catch (DateTimeParseException e) {
+            LOG.error(e.getMessage());
             throw new DateTimeFormatException("Wrong format of date/time");
         }
 
         return dateTime;
     }
 
-    public static String dateToString(LocalDateTime dateTime) throws DateTimeFormatException {
+    public static String dateToString(LocalDateTime dateTime) throws DateTimeException, IllegalArgumentException {
+
+        if (dateTime==null) throw new IllegalArgumentException();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         String stringDate = null;
+
         try {
             stringDate = dateTime.format(formatter);
-        } catch (DateTimeParseException e) {
-            throw new DateTimeFormatException("Wrong format of date/time");
+        } catch (DateTimeException dte) {
+            LOG.error(dte.getMessage());
+            throw new DateTimeException("Wrong value of year ,month, day, hour or minute");
         }
 
         return stringDate;
--- Service/test/com/diosoft/calendar/server/util/DateParserTest.java
+++ Service/test/com/diosoft/calendar/server/util/DateParserTest.java
@@ -2,13 +2,15 @@ package com.diosoft.calendar.server.util;
 
 import com.diosoft.calendar.server.exception.DateTimeFormatException;
 import org.junit.Test;
+
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import static org.junit.Assert.*;
 
 public class DateParserTest {
 
     @Test
-    public void testStringToDate() throws DateTimeFormatException {
+    public void testStringToDate() throws DateTimeFormatException, IllegalArgumentException {
 
         String stringDate = "2015-01-01 00:00";
         LocalDateTime expectedDateTime = LocalDateTime.of(2015,1,1,0,0);
@@ -19,20 +21,41 @@ public class DateParserTest {
     }
 
     @Test(expected = DateTimeFormatException.class)
-    public void testStringToDateWrongFormat() throws DateTimeFormatException {
+    public void testStringToDateWrongFormat() throws DateTimeFormatException, IllegalArgumentException {
 
         String stringDate = "2015-1-1 00:00";
         LocalDateTime actualDateTime = DateParser.stringToDate(stringDate);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testStringToDateWithNullArg() throws DateTimeFormatException, IllegalArgumentException {
+
+        String stringDate = null;
+        LocalDateTime actualDateTime = DateParser.stringToDate(stringDate);
+    }
+
     @Test
-    public void testDateToString() throws DateTimeFormatException {
+    public void testDateToString() throws DateTimeException, IllegalArgumentException {
 
         LocalDateTime dateTime = LocalDateTime.of(2015,1,1,0,0);
         String expectedStrindDateTime = "2015-01-01 00:00";
         String actualDateTime = DateParser.dateToString(dateTime);
 
         assertEquals(expectedStrindDateTime,actualDateTime);
+    }
 
+    @Test(expected = DateTimeException.class)
+    public void testDateToStringWrongFormat() throws DateTimeException, IllegalArgumentException {
+
+        LocalDateTime dateTime = LocalDateTime.of(2015,15,1,0,0);
+        String actualDateTime = DateParser.dateToString(dateTime);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDateToStringWithNullArg() throws DateTimeException, IllegalArgumentException {
+
+        LocalDateTime dateTime = null;
+        String actualDateTime = DateParser.dateToString(dateTime);
     }
 }
